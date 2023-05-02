54f6d9c0f5d87fc6dbb67e36275831d286d9aeb8
ROTEMYO <rotem.yoeli@gmail.com>
2023-05-02 16:33:06 +0300

    First Version

--- SiteScreen.py
+++ SiteScreen.py
@@ -4,6 +4,59 @@ from bs4 import BeautifulSoup
 import re
 import urllib.parse
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+from selenium.webdriver.common.by import By
+
+def download_chapters(url):
+    # Create a new instance of the Chrome driver
+    driver = webdriver.Chrome()
+
+    # Navigate to the website
+    driver.get(url)
+
+    # Find all the headings on the page and store them in an array
+    headings = driver.find_elements(By.XPATH, "//h1 | //h2 | //h3 | //h4 | //h5 | //h6")
+
+    # Find the text between each pair of headings and store it in an array of chapters
+    chapters = []
+    for i in range(len(headings) - 1):
+        start = headings[i]
+        end = headings[i + 1]
+        text = ""
+        current = start
+        while current != end:
+            try:
+                current = current.find_element(By.XPATH, "following-sibling::*[not(self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6)][1]")
+                if current.tag_name == "p":
+                    text += current.text + "\n\n"
+            except:
+                break
+        chapters.append(text)
+
+    # Close the browser
+    driver.quit()
+
+    # Return the array of chapters
+    return chapters
+
+def scrape_website_text(url):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    page_source = driver.execute_script('return document.body.innerHTML')
+    soup = BeautifulSoup(page_source, 'html.parser')
+    page_text = soup.get_text()
+    driver.quit()
+    return page_text
+
+
+
+
+
 def get_text(url):
     # Send a GET request to the website and get its HTML content
     response = requests.get(url)
@@ -17,8 +70,10 @@ def get_paragraphs(site_url):
     paragraphs = []
     visited_urls = set()
     queue = [site_url]
-    while queue:
+    counter = 0
+    while queue and counter < 100:
         url = queue.pop(0)
+        counter = counter + 1
         if url in visited_urls:
             continue
         visited_urls.add(url)
--- main.py
+++ main.py
@@ -2,22 +2,112 @@ import GetSentiment
 import SiteScreen
 import openai
 
-# text = "On the hunt for new hiking shoes? In 10 years, our all-female hiking team has tested 50 pairs, with the 12 best contenders now available in our current review. Our hiking enthusiasts have done leg work, busting out hundreds of long miles over the years through deserts, forests, mountains, and streams. We carry loaded packs, consider all-day comfort, and evaluate traction over and through wet, loose, and slippery surfaces. From differing foot shapes to varied trail conditions, we look at it all and rank each shoe according to our on-trail experiences. Over months of side-by-side comparison and testing, we tease apart the differences between these shoes and share our findings to help you pinpoint your perfect pair. Hiking is a world of fun in which you can either load up on gear or head out with nothing more than a stellar hydration pack and a good pair of shoes. But if you are the kind of hiker that loves to have all the top-notch hiking gear, we've probably tested it and have recommendations for you. If you are planning longer treks, carrying a heavy pack, or just prefer to have a bit more support, you might be interested in women's high-top hiking boots. If you're looking for men's hiking shoes, we've tested those too."
+import tkinter as tk
+from tkinter import scrolledtext
+from functools import partial
+import clipboard
 
-url = "https://www.switchbacktravel.com/best-tents-backpacking"
-paragraphs = SiteScreen.get_text(url)
-# paragraphs = SiteScreen.get_paragraphs("https://www.outravel.co.il/")
+def add_url(urls, url_entry):
+    # Get the text from the clipboard
+    clipboard_text = clipboard.paste()
+
+    # If there is text in the clipboard, insert it into the URL entry field
+    if clipboard_text:
+        url_entry.insert(tk.END, clipboard_text)
+
+    # Add the URL to the set of URLs and clear the entry field
+    urls.add(url_entry.get())
+    url_entry.delete(0, tk.END)
+    url_entry.focus()
+
+    # Update the clipboard with the URL that was added
+    clipboard.copy(url_entry.get())
+
+def process_urls(urls, output_text):
+    # Placeholder function that processes the URLs
+    output_text.insert(tk.END, "Processing URLs: " + str(urls) + "\n")
+
+def process_text(text, output_text):
+    # Placeholder function that processes the text
+    output_text.insert(tk.END, "Processing text: " + str(texts) + "\n")
+
+
+def add_text(texts, text_entry):
+    # Get the text from the clipboard
+    clipboard_text = clipboard.paste()
+
+    # If there is text in the clipboard, insert it into the text entry field
+    if clipboard_text:
+        text_entry.insert(tk.END, clipboard_text)
+
+    # Add the URL to the set of URLs and clear the entry field
+    texts.add(text_entry.get())
+    text_entry.delete(1, tk.END)
+    text_entry.focus()
+
+    # Update the clipboard with the URL that was added
+    clipboard.copy(text_entry.get())
+
+
+# Create the GUI window
+window = tk.Tk()
+window.title("Website and Text Processor")
+
+# Create a set to store the URLs and a set to store the texts
+urls = set()
+texts = set()
+
+# Create a text area to display the output
+output_text = scrolledtext.ScrolledText(window, width=40, height=10)
+output_text.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+
+# Create a label and entry field for adding URLs
+url_label = tk.Label(window, text="Add URL:")
+url_label.grid(row=0, column=0, padx=5, pady=5)
+url_entry = tk.Entry(window, width=30, exportselection=0, selectbackground="light blue")
+url_entry.grid(row=0, column=1, padx=5, pady=5)
+url_button = tk.Button(window, text="Add", command=partial(add_url, urls, url_entry))
+url_button.grid(row=0, column=2, padx=5, pady=5)
+
+# Create a label and entry field for adding text
+text_label = tk.Label(window, text="Add text:")
+text_label.grid(row=1, column=0, padx=5, pady=5)
+text_entry = tk.Entry(window, width=30, exportselection=0, selectbackground="light blue")
+text_entry.grid(row=1, column=1, padx=5, pady=5)
+text_button = tk.Button(window, text="Add", command=partial(add_text, texts, text_entry))
+text_button.grid(row=1, column=2, padx=5, pady=5)
+
+# Create a button to process the URLs and texts
+process_button = tk.Button(window, text="Process", command=lambda: [process_urls(urls, output_text), process_text(texts.pop(), output_text)])
+process_button.grid(row=2, column=1, padx=5, pady=5)
+
+# Start the GUI event loop
+window.mainloop()
+
+
+
+
+'''
+url = "https://www.rei.com/learn/expert-advice/best-approach-shoes.html"
+#paragraphs = SiteScreen.scrape_website_text(url)
+
+paragraphs = SiteScreen.download_chapters(url)
+# paragraphs = SiteScreen.get_paragraphs(url)
 
 totalScore = 0
 counter = 0
-texts = ["Big Agnes Copper", "Nemo Kunai", "Salomon Outward Mid GTX"]
+texts = ["Big Agnes Copper", "Zpacks Duplex", "Nemo Dagger"]
 
 # Loop through each paragraph and print the sentiment score
 for paragraph in paragraphs:
     counter = counter+1
     score, sentiment = GetSentiment.analyze_sentiment(paragraph)
-    totalScore = (totalScore + score) / counter
+    #totalScore = (totalScore + score) / counter
+    if score > 0.5:
+        print(f"Sentiment score: {score}\nSentiment: {sentiment}\nText: {paragraph}")
+
     for text in texts:
         if text in paragraph:
             print(f"Sentiment score: {score}\nSentiment: {sentiment}\nText: {paragraph}")
 
+'''
